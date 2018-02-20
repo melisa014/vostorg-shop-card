ced91869580a3caf18d9ec4bb6a4ce2590a4d0e5
melisa014 <mikhailovajk@mail.ru>
2018-02-20 22:14:37 +0300

    переопределена форма создания продукта

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -5,6 +5,10 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ProductType extends AbstractType
 {
@@ -14,13 +18,13 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')
-                ->add('vendorCode')
-                ->add('description')
-                ->add('price')
-                ->add('category')
-                ->add('colors');
-//                ->add();
+        $builder->add('name', TextType::class)
+                ->add('vendorCode', TextType::class)
+                ->add('description', TextareaType::class)
+                ->add('price', TextType::class)
+                ->add('category', ChoiceType::class)
+                ->add('colors', ChoiceType::class)
+                ->add('photo', FileType::class);
     }
     
     /**
@@ -28,9 +32,9 @@ class ProductType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'data_class' => 'AppBundle\Entity\Product'
-        ));
+        ]);
     }
 
     /**
