2020a7cf986b0cfca8cef91f2b471092b1310bff
melisa014 <mikhailovajk@mail.ru>
2018-04-18 17:37:35 +0300

    cnhfyица продукта

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -20,6 +20,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class ProductController extends Controller
 {
     /**
+     * @Route("/product/{productId}", name="product_page")
+     * 
+     * @param int $productId
+     * 
+     * @return Response
+     */
+    public function createPageAction(int $productId, FirmGetter $firmGetter): Response
+    {
+        $em = $this->getDoctrine()
+                ->getManager();
+        
+        $product = $em->getRepository(Product::class)
+                ->find($productId);
+        
+        if (empty($product)) {
+            throw new EntityNotFoundException("Продукт с id = $productId не найден");
+        }
+        
+        return $this->render('product/page.html.twig',[
+            'firms' => $firmGetter->getAll(),
+            'product' => $product,
+        ]);
+    }
+    
+    /**
      * Lists all product entities.
      *
      * @Route("/", name="product_index")
