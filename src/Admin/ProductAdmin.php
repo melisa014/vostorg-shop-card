abfd2718f94e3245ab1f710feaf997499b746220
melisa014 <mikhailovajk@mail.ru>
2018-07-11 19:02:47 +0300

    добавлены поля в админку продукта

--- src/Admin/ProductAdmin.php
+++ src/Admin/ProductAdmin.php
@@ -2,27 +2,51 @@
 
 namespace App\Admin;
 
-
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProductAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('name', TextType::class);
+        $formMapper->add('name', TextType::class)
+            ->add('vendorCode', TextType::class)
+            ->add('description', TextType::class)
+            ->add('price', TextType::class)
+//            ->add('updatedAt', TextType::class)
+            ->add('category', ChoiceType::class)
+            ->add('firm', ChoiceType::class)
+            ->add('photos', TextType::class)
+            ->add('colors', ChoiceType::class);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('name');
+        $datagridMapper->add('name')
+            ->add('vendorCode')
+            ->add('description')
+            ->add('price')
+//            ->add('updatedAt')
+            ->add('category')
+            ->add('firm')
+            ->add('photos')
+            ->add('colors');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('name');
+        $listMapper->addIdentifier('name')
+            ->add('vendorCode')
+            ->add('description')
+            ->add('price')
+//            ->add('updatedAt')
+            ->add('category')
+            ->add('firm')
+            ->add('photos')
+            ->add('colors');
     }
 }
