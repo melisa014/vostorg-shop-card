503ea1234511a6b74df15c25541e8aa4c27bbf6f
melisa014 <mikhailovajk@mail.ru>
2018-08-18 15:48:09 +0300

    обрезка картинок работает через get-параметры

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Service\FirmGetter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use ItForFree\rusphp\File\Image\ImageResizer;
 
 class ImageController extends Controller
@@ -15,19 +13,7 @@ class ImageController extends Controller
      * @Route("/image", name="image")
      */
     public function showAction(Request $request)
-//    @Route("/image/{path}/{format}", name="image")
-//    public function showAction(string $path, string $format)
     {
-        // ToDo: научиться передавать параметры из шаблона
-//        dump($path);
-//        dump($format);
-//        die();
-        dump($request->get('path'));
-        dump($request->get('format'));
-        die;
-
-        ImageResizer::showInFormat($path, $format);
-
-//        return new Response(Response::HTTP_OK);
+        ImageResizer::showInFormat($request->get('path'), $request->get('format'));
     }
 }
