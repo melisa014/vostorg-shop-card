bdcd2008a46b17b763e3dfb67e99b23047761eb4
Julia Mikhailova <j.mikhailova@dymov.tech>
2018-10-30 15:08:49 +0300

    изменён листенер ошибок

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -23,33 +23,18 @@ class ExceptionListener
 
         $response = new Response();
 
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->setContent(
-                json_encode(
-                    [
-                        'code' => $exception->getCode(),
-                        'message' => $exceptionMessage,
-                    ],
-                    256 | 128
-                )
-            );
-
-            $response->headers->replace($exception->getHeaders());
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-            $response->setContent(
-                json_encode(
-                    [
-                        'code' => '00',
-                        // TODO: нужно дописывать или не дописывать ошибку в зависимости от окружения
-                         'message' => "Внутренняя ошибка сервера. $exceptionMessage",
+        $response->setStatusCode($exception->getStatusCode());
+        $response->setContent(
+            json_encode(
+                [
+                    'code' => '00',
+                    // TODO: нужно дописывать или не дописывать ошибку в зависимости от окружения
+                     'message' => "Внутренняя ошибка сервера. $exceptionMessage",
 //                        'message' => "Внутренняя ошибка сервера",
-                    ],
-                    256 | 128
-                )
-            );
-        }
+                ],
+                256 | 128
+            )
+        );
 
         $event->setResponse($response);
     }
