e5847afa54c609e0efc3c1964a5c36bc961dd83d
Julia Mikhailova <j.mikhailova@dymov.tech>
2018-12-01 18:40:19 +0300

    карта показывается но только дя разработки

--- public/js/agency.js
+++ public/js/agency.js
@@ -42,28 +42,8 @@
   // Hide navbar when modals trigger
   $('.portfolio-modal').on('show.bs.modal', function(e) {
     $(".navbar").addClass("d-none");
-  })
+  });
   $('.portfolio-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
-  })
-  
-  function initMap() {
-    // Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-    var centerLatLng = new google.maps.LatLng(51.656996, 39.159271);
-    // Обязательные опции с которыми будет проинициализированна карта
-    var mapOptions = {
-            center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-            zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
-    };
-    
-    // Создаем карту внутри элемента #map
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: centerLatLng, map: map});
-    }
-    
-    // Ждем полной загрузки страницы, после этого запускаем initMap()
-    google.maps.event.addDomListener(window, "load", initMap);
-
+  });
 })(jQuery); // End of use strict
